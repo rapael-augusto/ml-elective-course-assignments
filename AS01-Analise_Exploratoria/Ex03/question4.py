cc13affe2186dfeb4bf9303680b2397611124d7d
Raphael Augusto <augustorapha27@gmail.com>
2026-05-26 16:47:24 -0300

    minor fix: change the columns array slicing to [1:] instead of [:-1]

--- AS01-Analise_Exploratoria/Ex03/question4.py
+++ AS01-Analise_Exploratoria/Ex03/question4.py
@@ -9,10 +9,11 @@ df = pd.read_csv("../databases/wine/wine.data", names=columns)
 fig, axes = plt.subplots(3, 5, figsize=(20, 15))
 axes = axes.flatten()
 
-for i, var in enumerate(columns[:-1]):
+for i, var in enumerate(columns[1:]):
     sns.violinplot(data=df, x='class', y=var, ax=axes[i])
     axes[i].set_xlabel('class')
     axes[i].set_ylabel('value')
+    axes[i].set_title(var)
 
 for i in range(len(columns[1:]), len(axes)):
     fig.delaxes(axes[i])
